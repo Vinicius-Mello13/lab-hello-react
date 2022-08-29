da5b3deee22d5902096993bdb3c29a1eaa88b27f
Vinicius Mello <vinicius.dallavecchia@yahoo.com.br>
2022-08-29 16:40:59 -0300

    done

---
+++ hello-react/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Footers from "./Components/Footer/Footers"
+import Headers from "./Components/Header/Headers"
+
+export default function App() {
+  return(
+    <>   
+    <Headers/>
+    <Footers/>
+    </>
+  )
+}
